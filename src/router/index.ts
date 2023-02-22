8077f17023c592701a126b3bf8a19893f2628a20
Damien Le Dantec <damien-le-dantec@hotmail.com>
2023-02-22 10:44:41 +0100

    Fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const PreparationView = () => import("../views/PreparationView.vue");
 const ShoppingView = () => import("../views/ShoppingView.vue");
 
 const router = createRouter({
-  history: createWebHistory("/fishing-log/"),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
