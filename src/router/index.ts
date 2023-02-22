a2f9e64fa95768f3270eebc6c01899593b400d57
Damien Le Dantec <damien-le-dantec@hotmail.com>
2023-02-22 11:14:59 +0100

    Fix subdirectory

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const PreparationView = () => import("../views/PreparationView.vue");
 const ShoppingView = () => import("../views/ShoppingView.vue");
 
 const router = createRouter({
-  history: createWebHistory("/"),
+  history: createWebHistory("/fishing-log/"),
   routes: [
     {
       path: "/",
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      injectRegister: "auto",
+    }),
+  ],
+  base: "/fishing-log/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
